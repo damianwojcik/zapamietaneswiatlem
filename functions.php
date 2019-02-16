4671011ef7a193823bebc83bed6137b19594247b
Damian Wójcik <damian.wojcik@agora.pl>
2019-02-16 19:22:58 +0100

    singular page template, plugins custom css

--- functions.php
+++ functions.php
@@ -5,10 +5,13 @@
 	add_theme_support( 'post-thumbnails' );
 	add_theme_support( 'menus' );
 
+	add_theme_support( 'post-formats', array( 'gallery' ) );
+
 	add_action( 'wp_enqueue_scripts', 'enqueue_styles' );
 	add_action( 'wp_footer', 'enqueue_scripts' );
 
 	add_image_size( 'post-thumb', 745, 400 );
+	add_image_size( 'post-title', 1920, 1080 );
 
 	show_admin_bar(false);
 
--- singular.php
+++ singular.php
@@ -1,5 +1,50 @@
 <?php get_header(); ?>
 
+    <!-- Title Header -->
+    <section class="nbr-page-section">
+        <div class="container">
+            <div class="col-md-8 offset-md-2">
+                <div class="nbr-page-content text-center">
+                    <h2><?php echo get_the_title(); ?></h2>
+                    <div class="nbr-meta-info">
+                        <span><i class="ti-time"></i> <?php echo get_the_date(); ?></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
+    <!-- Post Section -->
+    <?php
+        $image = wp_get_attachment_image_src( get_post_thumbnail_id(), 'post-title' );
+    ?>
+    <section class="nbr-blog-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="nbr-blog-items nbr-single-post">
+                       <div class="entry-thumb">
+                           <img src="<?php echo $image[0]; ?>" alt="<?php echo get_the_title(); ?>">
+                       </div>
+
+                       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+                        <?php the_content(); ?>
+
+                        <?php echo do_shortcode('[DISPLAY_ULTIMATE_SOCIAL_ICONS]'); ?>
+
+                        <?php
+                            if(is_single()) {
+                                comments_template();
+                            }
+                        ?>
+
+                        <?php endwhile; endif; ?>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 <?php get_footer(); ?>
